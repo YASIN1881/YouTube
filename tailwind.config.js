176f1f7353fcf9e0325d6b94c40df815c2553a04
YasinDev <yasinnasiry18@gmail.com>
2025-01-18 10:50:11 +0330

    Inaitial Color Palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: 'class', // for Dark mode
   content: ['./index.html', './src/**/*.{js,jsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'gradient':'linear-gradient(90deg, #FF0033 0%, #F50057 100%)',
+        'pink-gradient': 'linear-gradient(90deg, rgba(255,255,255,0.3) 0%, rgba(255,0,128,1) 100%)',
+        'red':'#FF0033',
+        'redBackground': 'linear-gradient(90deg, rgba(255,255,255,0.06) 0%, rgba(255,0,51,0.06) 100%)',
+        'redButton': 'linear-gradient(to right, rgba(255, 255, 255, 0.2), rgba(0, 0, 0, 0.2), rgba(255, 50, 91, 1))',
+        'redLight': '#FFAABB',
+        'backgroundSelected': '#FFAABB'
+      }
+    },
   },
   plugins: [],
 }
