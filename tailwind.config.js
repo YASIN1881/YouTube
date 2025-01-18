6510f95383fd82113945f0deb8159a552db789f7
YasinDev <yasinnasiry18@gmail.com>
2025-01-18 10:25:17 +0330

    Initial Project

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: 'class', // for Dark mode
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
